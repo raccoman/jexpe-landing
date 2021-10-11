d622660ade5619afdd6e9e3f9679f21f071332c3
Riccardo Accomando <riccardoaccoma@gmail.com>
2021-10-11 18:57:33 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>        
-      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=UA-209924572-1">
+      <Script id="google-tag-manager" strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=UA-209924572-1">
       </Script>
       <Script strategy="lazyOnload" id="google-analytics">
         {
