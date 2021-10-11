05d5c8d554e46c17b4503340375211cf61ccb085
Riccardo Accomando <riccardoaccoma@gmail.com>
2021-10-11 18:56:07 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>        
-    <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=UA-209924572-1">
-    </Script>
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=UA-209924572-1">
+      </Script>
+      <Script strategy="lazyOnload" id="google-analytics">
         {
           `
-      window.dataLayer = window.dataLayer || [];
+          window.dataLayer = window.dataLayer || [];
 
       function gtag() {
         dataLayer.push(arguments);
