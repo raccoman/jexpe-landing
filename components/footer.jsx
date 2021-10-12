4fccc2950460add96ff9a50a82dd96fceef7d7ec
Riccardo Accomando <riccardoaccoma@gmail.com>
2021-10-12 21:37:27 +0200

    rel=noreferrer

--- components/footer.jsx
+++ components/footer.jsx
@@ -37,7 +37,7 @@ function Footer() {
             ].map((value, index) => {
               return (
                 <li key={index}>
-                  <a target="_blank" href={value.link}>{value.text}</a>
+                  <a target="_blank" rel="noreferrer" href={value.link}>{value.text}</a>
                 </li>
               );
             })}
