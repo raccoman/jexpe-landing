e2994125a13f7d0644972d9fb7d82b3839df1f4a
Riccardo Accomando <riccardoaccoma@gmail.com>
2023-05-24 00:54:22 +0200

    refactor: homepage UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ import { GithubLogo, PaperPlaneTilt } from '@phosphor-icons/react'
 const HomePage: NextPage = () => {
 
     return (
-        <div className='h-screen w-screen p-6 sm:p-12 flex'>
+        <div className='h-screen w-screen p-6 xs:p-12 flex'>
 
-            <div className='h-full flex flex-col justify-between w-full'>
+            <div className='h-full w-full flex flex-col gap-52 xs:justify-between'>
 
                 <div className='flex justify-between items-center'>
                     <p>Jexpe</p>
@@ -22,26 +22,26 @@ const HomePage: NextPage = () => {
 
                 <div className='flex flex-col gap-20'>
 
-                    <p className='text-4xl sm:text-7xl font-semibold'>
+                    <p className='text-4xl xs:text-7xl font-semibold'>
                         The alternative agency for<br />
                         alternative brands.
                     </p>
 
                     <div className='flex flex-col'>
-                        <p className='uppercase font-extrabold text-xs lg:text-2xs text-gray-700'>
+                        <p className='uppercase font-extrabold text-xs text-gray-700'>
                             CONTACT US
                         </p>
                         <p className='mt-1 font-light text-gray-300 leading-snug'>
                             Need a stunning website? Get a quote now!
                         </p>
                         <div className='mt-4 flex items-center'>
-                            <p className='bg-gray-800 px-2 sm:px-4 h-8 sm:h-10 rounded-l text-sm flex items-center'>
+                            <p className='bg-gray-800 px-2 xs:px-4 h-10 rounded-l text-sm flex items-center'>
                                 {CONTACT_EMAIL}
                             </p>
                             <Link href={`mailto:${CONTACT_EMAIL}`}
-                                  className='rounded-r flex items-center text-black/75 hover:bg-[#97edff] bg-[#b1ff9e] px-2 sm:px-4 h-full uppercase font-extrabold text-xs'>
-                                <span className='hidden sm:flex'>SEND EMAIL</span>
-                                <PaperPlaneTilt weight='duotone' size={16} className='flex sm:hidden' />
+                                  className='rounded-r flex items-center text-black/75 hover:bg-[#97edff] bg-[#b1ff9e] px-2 xs:px-4 h-full uppercase font-extrabold text-xs'>
+                                <span className='hidden xs:flex'>SEND EMAIL</span>
+                                <PaperPlaneTilt weight='duotone' size={16} className='flex xs:hidden' />
                             </Link>
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-  },
-  plugins: [],
+    content: [
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: '350px',
+            },
+        },
+    },
+    plugins: [],
 }
